569d7eed32c3ccc0457cf90dc456a54b342d2040
chrishasbeard <chrisholmes16@gmail.com>
2019-10-29 07:02:36 -0400

    Capstone 2

---
+++ Capstone2/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+namespace Capstone2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                PrintOptions();
+                string userInput = GetUserInput("Please choose one of the 5 options: ");
+                SelectMenuOption(userInput);
+            }
+        }
+        public static void CreateTask()
+        {
+            Task task = new Task();
+            task.Name = GetUserInput("Enter Name:");
+            task.Description = GetUserInput("Enter Decription: ");
+            task.DueDate = ValiDate("Enter Due Date: ");
+
+            Task.AddTask(task);
+      
+            Task.DisplayTask();
+        }
+        public static DateTime ValiDate(string message)
+        {
+            try
+            {
+                return DateTime.Parse(GetUserInput(message));
+            }
+            catch
+            {
+                Console.WriteLine("That isn't a valid input.");
+                return ValiDate(message);
+            }
+        }
+
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        public static void PrintOptions()
+        {
+            Console.WriteLine("1. List Tasks");
+            Console.WriteLine("2. Add Task");
+            Console.WriteLine("3. Delete Task");
+            Console.WriteLine("4. Mark task complete");
+            Console.WriteLine("5. Quit");
+        }
+
+        public static void SelectMenuOption(string userInput)
+        {
+            switch (userInput)
+            {
+                case "1":
+                case "list":
+                    Task.DisplayTask();
+                    break;
+
+                case "add task":
+                case "2":
+                case "add":
+                    CreateTask();
+                    break;
+
+                case "3":
+                case "delete task":
+                    RemoveTask();
+                    break;
+
+                case "4":
+                case "mark task complete":
+                    MarkComplete();
+                    break;
+
+                case "5":
+                case "quit":
+                    Console.WriteLine("\n\n\t\tLater, N E R D !!!!");
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("\n\tGet Good, and try again when amateur hour is over.\n");
+
+                    break;
+            }
+        }
+        public static void RemoveTask()
+        {
+            string userInput = GetUserInput("Enter A Task Number to Remove.");
+            int input = int.Parse(userInput);
+
+            Task.DeleteTask(input);
+            Console.Clear();
+
+        }
+        public static void MarkComplete()
+        {
+            int userInput = 0;
+            Task.DisplayTask();
+            try
+            {
+                userInput = int.Parse(GetUserInput("Select Task Number to Complete: "));
+            }
+            catch (FormatException)
+            {
+                MarkComplete();
+            }
+
+            Task.CompleteTask(userInput);
+            Task.DisplayTask();
+        }
+    }
+}
---
+++ Capstone2/Task.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone2
+{
+    class Task
+    {
+        private static List<Task> _tasks = new List<Task>
+        {
+            new Task ("Jack", "Does the work", DateTime.Parse("10/25/2019")),
+            new Task ("Tristan", "Stares Blankly", DateTime.Parse("10/24/2019")),
+            new Task ("Steve", "Does nothing", DateTime.Parse("10/25/2019"))
+        };
+        #region fields
+        //fields
+        private string _name;
+        private string _description;
+        private DateTime _dueDate;
+        private bool _complete;
+        #endregion
+
+        #region properties
+        //properties per andy's say so
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public DateTime DueDate
+        {
+            get { return _dueDate; }
+            set { _dueDate = value; }
+        }
+        public bool Complete
+        {
+            get { return _complete; }
+            set { _complete = value; }
+        }
+
+        #endregion
+      
+
+        public Task() { }
+
+        public Task(string name, string description, DateTime dueDate, bool complete = false)
+        {
+            Name = name;
+            Description = description;
+            DueDate = dueDate;
+            Complete = complete;
+        }
+        public static void DisplayTask()
+        {
+            for (int i = 0; i < _tasks.Count; i++)
+            {
+               
+                Console.WriteLine($"\t{i + 1}: {_tasks[i].Name}");
+                Console.WriteLine($"\tWhich does: {_tasks[i].Description}");
+                Console.WriteLine($"\tWhich is due: {_tasks[i].DueDate}");
+                Console.WriteLine($"\tCompleted?: {_tasks[i].Complete}\n");
+
+            }
+        }
+
+        public static void CompleteTask(int userInput)
+        {
+            _tasks[userInput - 1].Complete = true;
+        }
+
+        public static void DeleteTask(int userInput)
+        {
+            _tasks.RemoveAt(userInput-1);
+        }
+
+
+        public static void AddTask(Task task)
+        {
+            _tasks.Add(task);
+        }
+
+    }
+}
